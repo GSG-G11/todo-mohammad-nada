481b97c55a11a3139f7d48d4b4d0680b52558014
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-04-07 02:53:41 +0300

    Format Date and Time

--- src/components/Modal/Card.jsx
+++ src/components/Modal/Card.jsx
@@ -5,12 +5,45 @@ export default function Card({
   changeCheck,
   deleteTask,
 }) {
+  let formatedTime = '';
+  // Format Date
+  const formatDate = (taskDate) => {
+    let d = new Date(taskDate),
+      mo = new Intl.DateTimeFormat('en', {
+        month: 'short',
+      }).format(d),
+      da = new Intl.DateTimeFormat('en', {
+        day: '2-digit',
+      }).format(d);
+    let getDateFormat = `${da}-${mo}`;
+    return getDateFormat;
+  };
+
+  // Format Time
+  const formatTime = (taskTime) => {
+    let timeformat = taskTime.split(':'),
+      hours = timeformat[0],
+      minutes = timeformat[1],
+      ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    let strTime = `${hours}:${minutes} ${ampm}`;
+    return strTime;
+  };
+
+  // Format Date and Time
+  if (time) {
+    formatedTime = `${formatDate(time.split('T')[0])}  ${formatTime(
+      time.split('T')[1]
+    )}`;
+  }
+
   return (
     <div className="card">
       <p>{title}</p>
       <p>{desc}</p>
       <p>{label !== 'select a label' && label}</p>
-      <p>{time}</p>
+      <p>{time && formatedTime} </p>
       <label>
         <input
           type="checkbox"
