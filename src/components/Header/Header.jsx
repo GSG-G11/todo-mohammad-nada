4d3ec87068ee8eba159c76b964027ec965d03ef6
MohammadAlHabil <mohammadalhabil1999@gmail.com>
2022-04-07 06:20:25 +0300

    create filter tasks relates #15

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import logo from '../../logo.svg';
 export default function Header({ searchByTitle }) {
   return (
     <header>
-      <img src={logo} width="50px" height="50px" />
+      <img src={logo} width="50px" height="50px" alt="logo_image" />
       <Search searchByTitle={searchByTitle} />
       <p>Nada</p>
     </header>
