8c56c33e551dd657027df25874bda3ca0f175043
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-04-07 10:58:09 +0300

    add no task img

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,6 @@
 import Card from './Card';
 import './Cards.css';
+import noTask from './noTask.svg';
 
 export default function Cards({
   tasks,
@@ -16,17 +17,25 @@ export default function Cards({
   return (
     <section className="wrap-cards">
       <ul className="grid-cards">
-        {tasks.map((task, index) => {
-          return (
-            <Card
-              key={index}
-              openModal={openModal}
-              task={task}
-              changeCheck={changeCheck}
-              deleteTask={deleteTask}
-            />
-          );
-        })}
+        {tasks.length ? (
+          tasks.map((task, index) => {
+            return (
+              <Card
+                key={index}
+                openModal={openModal}
+                task={task}
+                changeCheck={changeCheck}
+                deleteTask={deleteTask}
+              />
+            );
+          })
+        ) : (
+          <div className="no-task">
+            <img src={noTask} alt="no task" className="no-task-img" />
+            <p className="no-task-p">No Tasks</p>
+            <p className="no-task-desc">Start now to add your tasks</p>
+          </div>
+        )}
       </ul>
     </section>
   );
--- src/components/Nav/Controls.jsx
+++ src/components/Nav/Controls.jsx
@@ -1,23 +1,32 @@
+import {Component} from 'react'
 import './Nav.css';
-export default function Controls({
-  changeFilter,
-  openModal,
-  doneTasks,
-  unDoneTasks,
-  allTasks,
-}) {
+
+export default class Controls extends Component {
+  state = {
+    active: 'all'
+  }
+
+  isActive = (name) =>{
+      this.setState({active : name})
+  }
+
+  render(){
+
+  const {openModal, changeFilter, allTasks, unDoneTasks, doneTasks } = this.props;
+  const {active} = this.state;
+
   return (
     <nav className="nav">
-      <button className="add-task" onClick={() => openModal('Add')} value="Add">
+      <button className="add-task" onClick={() => openModal('Add')} value="Add" >
         <i className="fa-solid fa-plus"></i>Add
       </button>
-      <button onClick={allTasks} name="all" value="all">
+      <button onClick={() => { allTasks(); this.isActive('all')}} name="all" value="all" className={active === 'all' ? 'active' : 'btn'} >
         All
       </button>
-      <button onClick={unDoneTasks} name="undone" value="UnDone">
+      <button onClick={() => { unDoneTasks(); this.isActive('undone')}} name="undone" value="UnDone" className={active === 'undone' ? 'active' : 'btn'}>
         UnDone
       </button>
-      <button onClick={doneTasks} name="done" value="Done">
+      <button onClick={() => { doneTasks(); this.isActive('done')}} name="done" value="Done" className={active === 'done' ? 'active' : 'btn'}>
         Done
       </button>
       <select name="filter" defaultValue="filter" onChange={changeFilter}>
@@ -28,4 +37,5 @@ export default function Controls({
       </select>
     </nav>
   );
+}
 }
